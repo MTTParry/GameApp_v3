9110bd859dce692c95f03fb7324a46c19a1b4e5b
MTTParry <michaela.t.t.parry@gmail.com>
2022-03-31 18:28:36 -0700

    json is string; working on button

--- client/src/components/Hangman.js
+++ client/src/components/Hangman.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import LetterButtons from "./LetterButtons";
 
+const initialWord = "";
+
 function Hangman() {
-  const [word, setWord] = useState([]);
+  const [word, setWord] = useState(initialWord);
   const [wordLength, setWordlength] = useState(0);
 
   useEffect(() => {
@@ -10,20 +12,21 @@ function Hangman() {
     // SQL: "SELECT word FROM words ORDER BY RANDOM() LIMIT 1"
     fetch("http://localhost:5005/api/words")
       .then((response) => response.json())
-      .then((word) => {
-        setWord(word[0]);
+      .then((data) => {
+        setWord(data[0].word);
       })
       .then(console.log("This is the json const:", word));
   }, []);
 
-  let tempWord = word.word;
-  console.log("Tempword=", tempWord);
-  console.log(typeof tempWord);
+  // let tempWord = word;
+  // console.log("Tempword=", tempWord);
+  // console.log(typeof tempWord);
   // figuring out how to actually have the game function
+
   let guessedLetters = [];
   // need to update this via LetterButtons component
 
-  console.log("Word =>", word.word);
+  console.log("Word =>", word);
   // console.log("Word.length =>", word.word.length);
 
   // function to compare guessedLetters to {word.word}
@@ -38,12 +41,16 @@ function Hangman() {
 
   // updateWord();
 
+  function addGuess(letter) {
+    console.log(letter);
+  }
+
   return (
     <div>
-      <div>{word.word}</div>
+      <div>{word}</div>
       <br />
       <fieldset>
-        <LetterButtons />
+        <LetterButtons callback={addGuess} />
       </fieldset>
     </div>
   );
--- client/src/components/LetterButtons.js
+++ client/src/components/LetterButtons.js
@@ -1,25 +1,41 @@
-
-
-const LetterButtons = () => {
+const LetterButtons = (props) => {
   let letters = "abcdefghijklmnopqrstuvwxyz";
   let splitLetters = letters.toUpperCase().split("");
 
+  // button onClick=> add to guessedLetters Array and send that back up to the parent
+  // guessed letters will be used to identify letters that are in the word
+  // if guessedLetter does not match a letter in the array:
+  // non-matching letters replaced with " _ "
+  // remainingGuesses--
+  // if remainingGuesses === 0
+  // player loses
+
+  const handleButtonClick = (e) => {
+    if (!props.callback) {
+      console.log("You didn't pass in a function");
+      return;
+    }
+    console.log(e);
+    props.callback(e);
+  };
 
   //Listing Letters
   return (
     <div>
-    {splitLetters.map((letter) => (
-        <button 
+      {splitLetters.map((letter) => (
+        <button
           className="letter-buttons"
           key={letter}
           value={letter}
-          >
-            <b>{letter}</b>
+          onClick={(e) => {
+            handleButtonClick(letter);
+          }}
+        >
+          <b>{letter}</b>
         </button>
-        ))
-      }    
+      ))}
     </div>
-  )
+  );
 };
 
 export default LetterButtons;
